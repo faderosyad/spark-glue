5207b24f1000672a241092f79226bac650dca694
fade.rosyad <fade.rosyad@advotics.com>
2020-01-29 18:23:37 +0700

    adding aws-glue-lib

--- ClientAdvocatesETL.py
+++ ClientAdvocatesETL.py
@@ -1,11 +1,15 @@
 import sys
 from awsglue.transforms import *
+from awsglue.transforms.apply_mapping import ApplyMapping
+from awsglue.transforms.field_transforms import SelectFields
+from awsglue.transforms.resolve_choice import ResolveChoice
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from pyspark.shell import sqlContext
 from pyspark.sql import *
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from database import *
 
 # Create by: Fade Khalifah Rosyad
 # Email: fade.rosyad@advotics.com
@@ -25,59 +29,54 @@ glueContext = GlueContext(sc)
 spark = glueContext.spark_session
 job = Job(glueContext)
 
-
 job.init(args['JOB_NAME'], args)
 
-# Defining Data Source
+# Defining Database are on database.py file
 
-## @type: DataSource
-## @args: [database = "advo-sbx-cluster", table_name = "adv_identity_client_advocates", transformation_ctx = "client_advocates"]
-## @return: client_advocates
-## @inputs: []
-client_advocates = glueContext.create_dynamic_frame.from_catalog(database = "advo-sbx-cluster",
-                                                                 table_name = "adv_identity_client_advocates",
-                                                                 transformation_ctx = "client_advocates")
-
-## @type: DataSource
-## @args: [database = "advo-sbx-cluster", table_name = "adv_identity_client_advocate_data", transformation_ctx = "client_advocate_data"]
-## @return: client_advocate_data
-## @inputs: []
-client_advocate_data = glueContext.create_dynamic_frame.from_catalog(database = "advo-sbx-cluster",
-                                                                     table_name = "adv_identity_client_advocate_data",
-                                                                     transformation_ctx = "client_advocate_data")
-
-## @type: DataSource
-## @args: [database = "advo-sbx-cluster", table_name = "adv_identity_advocates", transformation_ctx = "advocates"]
-## @return: advocates
-## @inputs: []
-advocates = glueContext.create_dynamic_frame.from_catalog(database = "advo-sbx-cluster",
-                                                          table_name = "adv_identity_advocates",
-                                                          transformation_ctx = "advocates")
-
-## @type: DataSource
-## @args: [database = "advo-sbx-cluster", table_name = "adv_identity_advocate_data", transformation_ctx = "advocates_data"]
-## @return: advocates_data
-## @inputs: []
-advocates_data = glueContext.create_dynamic_frame.from_catalog(database = "advo-sbx-cluster",
-                                                               table_name = "adv_identity_advocate_data",
-                                                               transformation_ctx = "advocates_data")
-
-## @type: DataSource
-## @args: [database = "advo-sbx-cluster", table_name = "adv_identity_user_logins", transformation_ctx = "user_logins"]
-## @return: user_logins
-## @inputs: []
-user_logins = glueContext.create_dynamic_frame.from_catalog(database = "advo-sbx-cluster",
-                                                            table_name = "adv_identity_user_logins",
-                                                            transformation_ctx = "user_logins")
-
-# Create ETL Process before Mapping to ApplyMapping
 # TODO: solve how to create a mapping without joinning table
+# 1. To create ETL, first create 1 new data pool
+# 2. after that, that data pool will be fill with data from dataframe
 
+# datapool = sqlContext.createDataFrame((client_advocates,
+#                                        client_advocate_data,
+#                                        client_roles,
+#                                        advocates,
+#                                        advocate_address,
+#                                        advocates_data,
+#                                        user_logins,
+#                                        company))
+
+dwRawClientAdvocateSchema = ['client_id', 'advocate_id', 'advocate_type', 'name', 'login_username', 'is_login_active', 'phone', 'is_login_active' ]
+
+# Made OLTP Client Advocate table as base for the datapool
+datapool = sqlContext.createDataFrame(client_advocates, dwRawClientAdvocateSchema)
+
+#search for role table
+
+
+# Mapping Process
 ## @type: ApplyMapping
 ## @args: [mapping = [("creation_time", "timestamp", "creation_time", "timestamp"), ("role", "string", "phone", "string"), ("advocate_id", "int", "advocate_id", "int"), ("sales_group_id", "int", "sales_group_id", "int"), ("xl_msisdn", "string", "province", "string"), ("outlet_name", "string", "outlet_name", "string"), ("created_by", "string", "regency", "string"), ("client_id", "int", "client_id", "int"), ("parent_advocate", "int", "parent_advocate", "int"), ("client_ref_id", "string", "client_ref_id", "string"), ("zone_id", "int", "creation_date_index", "int"), ("last_updated_by", "string", "advocate_data", "string"), ("last_updated_time", "timestamp", "client_advocate_creation_time", "timestamp"), ("advocate_type", "string", "advocate_type", "string"), ("work_entity", "int", "registered_by", "int"), ("status", "string", "status", "string"), ("loki_index", "long", "loki_index", "long")], transformation_ctx = "applymapping1"]
 ## @return: applymapping1
 ## @inputs: [frame = datasource0]
-applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("creation_time", "timestamp", "creation_time", "timestamp"), ("role", "string", "phone", "string"), ("advocate_id", "int", "advocate_id", "int"), ("sales_group_id", "int", "sales_group_id", "int"), ("xl_msisdn", "string", "province", "string"), ("outlet_name", "string", "outlet_name", "string"), ("created_by", "string", "regency", "string"), ("client_id", "int", "client_id", "int"), ("parent_advocate", "int", "parent_advocate", "int"), ("client_ref_id", "string", "client_ref_id", "string"), ("zone_id", "int", "creation_date_index", "int"), ("last_updated_by", "string", "advocate_data", "string"), ("last_updated_time", "timestamp", "client_advocate_creation_time", "timestamp"), ("advocate_type", "string", "advocate_type", "string"), ("work_entity", "int", "registered_by", "int"), ("status", "string", "status", "string"), ("loki_index", "long", "loki_index", "long")], transformation_ctx = "applymapping1")
+applymapping1 = ApplyMapping.apply(frame = datasource0, mappings = [("creation_time", "timestamp", "creation_time", "timestamp"),
+                                                                    ("role", "string", "phone", "string"),
+                                                                    ("advocate_id", "int", "advocate_id", "int"),
+                                                                    ("sales_group_id", "int", "sales_group_id", "int"),
+                                                                    ("xl_msisdn", "string", "province", "string"),
+                                                                    ("outlet_name", "string", "outlet_name", "string"),
+                                                                    ("created_by", "string", "regency", "string"),
+                                                                    ("client_id", "int", "client_id", "int"),
+                                                                    ("parent_advocate", "int", "parent_advocate", "int"),
+                                                                    ("client_ref_id", "string", "client_ref_id", "string"),
+                                                                    ("zone_id", "int", "creation_date_index", "int"),
+                                                                    ("last_updated_by", "string", "advocate_data", "string"),
+                                                                    ("last_updated_time", "timestamp", "client_advocate_creation_time", "timestamp"),
+                                                                    ("advocate_type", "string", "advocate_type", "string"),
+                                                                    ("work_entity", "int", "registered_by", "int"),
+                                                                    ("status", "string", "status", "string"),
+                                                                    ("loki_index", "long", "loki_index", "long")],
+                                   transformation_ctx = "applymapping1")
 
 ## @type: SelectFields
 ## @args: [paths = ["creation_time", "registered_by", "registered_date_index", "latitude", "is_login_active", "advocate_id", "regency", "latitude_index", "payment_terms", "outlet_name", "client_advocate_creation_time", "client_id", "creation_month_index", "province", "advocate_data", "sales_group_name", "credit_limit", "registered_month_index", "user_agent", "longitude", "phone_2", "contact_name", "address", "assigned_distributor_id", "login_username", "sales_group_id", "store_type", "org_role", "org_role_name", "parent_advocate", "client_ref_id", "phone", "name", "postal_code", "registered_date", "advocate_type", "phone_imei", "longitude_index", "status", "creation_date_index", "loki_index"], transformation_ctx = "selectfields2"]
